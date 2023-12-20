cdc11f072a5d20e4ee83bc1762a86b4ac00571c3
Myron Holmes <1dequanh@gmail.com>
2023-12-20 11:17:57 -0500

    student dto and mapper classes created

---
+++ src/main/java/com/myprojects/sms/mapper/StudentMapper.java
@@ -0,0 +1,26 @@
+package com.myprojects.sms.mapper;
+
+import com.myprojects.sms.dto.StudentDto;
+import com.myprojects.sms.entity.Student;
+
+public class StudentMapper {
+    public static StudentDto mapToStudentDto(Student student){
+        StudentDto studentDto = new StudentDto(
+                student.getId(),
+                student.getFirstName(),
+                student.getLastName(),
+                student.getEmail()
+        );
+        return studentDto;
+    }
+
+    public static Student mapToStudent(StudentDto studentDto){
+        Student student = new Student(
+                studentDto.getId(),
+                studentDto.getFirstName(),
+                studentDto.getLastName(),
+                studentDto.getEmail()
+        );
+        return student;
+    }
+}
