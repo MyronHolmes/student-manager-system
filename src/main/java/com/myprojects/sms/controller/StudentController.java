450d971c33da64a0a8aac5cdf05e02f671e377ae
Myron Holmes <1dequanh@gmail.com>
2023-12-20 14:46:24 -0500

    create_student form created

--- src/main/java/com/myprojects/sms/controller/StudentController.java
+++ src/main/java/com/myprojects/sms/controller/StudentController.java
@@ -23,4 +23,10 @@ public class StudentController {
         model.addAttribute("students", students);
         return "students";
     }
+    @GetMapping("/students/new")
+    public String newStudent(Model model){
+        StudentDto studentDto = new StudentDto();
+        model.addAttribute("student", studentDto);
+        return "create_student";
+    }
 }
