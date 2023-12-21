b5450edf2ed8cc6ac72c4bbd7ebb228599ac6559
Myron Holmes <1dequanh@gmail.com>
2023-12-20 19:25:58 -0500

    saveStudent feature created

--- src/main/java/com/myprojects/sms/controller/StudentController.java
+++ src/main/java/com/myprojects/sms/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.myprojects.sms.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -28,5 +30,11 @@ public class StudentController {
         StudentDto studentDto = new StudentDto();
         model.addAttribute("student", studentDto);
         return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") StudentDto student){
+        studentService.createStudent(student);
+        return "redirect:/students";
     }
 }
--- src/main/java/com/myprojects/sms/service/StudentService.java
+++ src/main/java/com/myprojects/sms/service/StudentService.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface StudentService {
     List<StudentDto> getAllStudents();
 
+    void createStudent(StudentDto student);
 }
